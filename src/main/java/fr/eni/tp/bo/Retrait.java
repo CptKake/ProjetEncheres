27f70a8ada693bb64650a60eaa171602c8596302
Alexis <kofskin@hotmail.fr>
2025-02-10 11:44:10 +0100

    creation of the Retrait class

---
+++ src/main/java/fr/eni/tp/bo/Retrait.java
@@ -0,0 +1,67 @@
+package fr.eni.tp.bo;
+
+public class Retrait {
+
+	private int number;
+	private String street;
+	private String postalCode;
+	private String city;
+	
+	public Retrait(String street, String postalCode, String city) {
+		this.street = street;
+		this.postalCode = postalCode;
+		this.city = city;
+	}
+
+	/**
+	 * @return the street
+	 */
+	public String getStreet() {
+		return street;
+	}
+
+	/**
+	 * @param street the street to set
+	 */
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	/**
+	 * @return the postalCode
+	 */
+	public String getPostalCode() {
+		return postalCode;
+	}
+
+	/**
+	 * @param postalCode the postalCode to set
+	 */
+	public void setPostalCode(String postalCode) {
+		this.postalCode = postalCode;
+	}
+
+	/**
+	 * @return the city
+	 */
+	public String getCity() {
+		return city;
+	}
+
+	/**
+	 * @param city the city to set
+	 */
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	/**
+	 * @return the number
+	 */
+	public int getNumber() {
+		return number;
+	}
+	
+	
+	
+}
