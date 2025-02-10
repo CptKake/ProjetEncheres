d40e96a63da5c692d3860f6dd0adac96642dd90a
Kevin <kevindelalande@hotmail.fr>
2025-02-10 14:19:37 +0100

    debut DAL

---
+++ src/main/java/fr/eni/tp/dal/ArticleDAO.java
@@ -0,0 +1,12 @@
+package fr.eni.tp.dal;
+
+import fr.eni.tp.bo.Article;
+
+public interface ArticleDAO {
+
+	void createArticle (Article article);
+	
+	void suppressArticle ( Article article);
+	
+	
+}
