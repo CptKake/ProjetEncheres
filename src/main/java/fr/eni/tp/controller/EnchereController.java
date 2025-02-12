ccb14883f03ac3b4b36dc8828db1b86e32546efa
Kevin <kevindelalande@hotmail.fr>
2025-02-12 09:34:30 +0100

    tests

--- src/main/java/fr/eni/tp/controller/EnchereController.java
+++ src/main/java/fr/eni/tp/controller/EnchereController.java
@@ -3,21 +3,28 @@ package fr.eni.tp.controller;
 import fr.eni.tp.bll.EnchereService;
 import fr.eni.tp.bo.Enchere;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/encheres")
 public class EnchereController {
-/*
+
     private final EnchereService enchereService;
 
     public EnchereController(EnchereService enchereService) {
         this.enchereService = enchereService;
     }
 
+    @GetMapping
+    public String index () {
+    	return "index";
+    }
+    
+    /*
     // Récupérer toutes les enchères
     @GetMapping
     public List<Enchere> getAllEncheres() {
