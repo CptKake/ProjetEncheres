8c5895ea6f2d82ca131ac09d4958ff0d8acc3517
Alexis <kofskin@hotmail.fr>
2025-02-14 11:38:49 +0100

    message

--- src/main/java/fr/eni/tp/dal/ArticleDAOImpl.java
+++ src/main/java/fr/eni/tp/dal/ArticleDAOImpl.java
@@ -4,7 +4,6 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
